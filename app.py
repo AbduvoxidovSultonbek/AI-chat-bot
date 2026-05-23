82fabb44ffa67652ef9d619e28f5a8a886c474cc
AbduvoxidovSultonbek <abduvohidovsultonbek585@gmail.com>
2026-05-23 13:09:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,22 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = Flask(__name__)
 
-chat_history = []
-
-@app.route("/chat", methods=["POST"])
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    user_message = request.json["message"]
+    data = request.get_json()
+    user_message = data.get("message")
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=user_message
     )
 
-    return jsonify({
-        "reply": response.text
-    })
+    return jsonify({"reply": response.text})
 
 
 if __name__ == "__main__":
